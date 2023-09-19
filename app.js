8c26f34baf158b3515ea9e9d247b9f8cdd96ac27
Vincent Serem <seremvincent19@gmail.com>
2023-09-19 08:34:40 +0300

    added caregiver routes

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const residentsRouter = require("./routes/residents")
 const authRouter = require("./routes/auth")
+const caregiverRoutes =  require("./routes/caregivers")
 
 var app = express();
 
@@ -56,6 +57,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/auth", authRouter)
 app.use("/resident", residentsRouter)
+app.use("/caregiver",caregiverRoutes)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/caregivers.js
+++ routes/caregivers.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET New CareGiver Page. */
 router.get('/new', function (req, res) {
-    res.render('caregiver.njk', { title: 'ACMS | New CareGiver' });
+    res.render('newcaregiver.njk', { title: 'ACMS | New CareGiver' });
 });
 
 
